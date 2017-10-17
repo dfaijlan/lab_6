b64e1ebfb8493e8ee3c52a54fd4091fdbdf62099
Dominic Fajilan <dfajilan@csumb.edu>
2017-10-17 21:48:40 +0000

    Lab 5

--- index.php
+++ index.php
@@ -1,43 +1,59 @@
 <!DOCTYPE html>
 <html>
     <head>
-        
+        <title>Database Search</title>
     </head>
-    
-    
     <body>
         <?php
         
         // connec tto our mysql database server
 
-        $sername = 'localhost';
-        $username = 'dfajilan';
-        $password = 'E11ipsis';
-        $dbname = 'tech_devices_app';
+        $sername = 'us-cdbr-iron-east-05.cleardb.net';
+        $username = 'b05742e5500b2b';
+        $password = 'a2d90cd8';
+        $dbname = 'heroku_e11c5f3d0a9fe08';
         
         $conn = new mysqli($sername, $username, $password, $dbname);
         
-        if($conn->connect_error)
-        {
-            die("Connection failed: ".$conn->connect_error);
-        }
-        echo"Connection Successful";
+        // if($conn->connect_error)
+        // {
+        //     die("Connection failed: ".$conn->connect_error);
+        // }
+        // echo"Connection Successful";
         
         
         // make a query
-        $sql = "SELECT id, name, college FROM Departments";
+        $sql = "SELECT * FROM device ORDER BY deviceName";
         $result = $conn->query($sql);
         if($result->num_rows > 0){
             while($row = $result->fetch_assoc())
             {
-                echo "department id: ".$row['id']. "name: " .$row['name']. " ". $row['college']."<br />";
+                echo "Id: " . $row['id'] . " Device: ". $row['deviceName'] . " / Type: " . $row['deviceType'] . " / Availability: " . $row['status'] . "<br />";
             }
         }
         else{
             echo "0 results";
         }
+        ?>
         
+        <form>
+            <br>Filter by: 
+            <select name="filter">
+                <option value="">Select One</option>
+                <option value="name">Name</option>
+                <option value="type">Type</option>
+                <option value="availability">Availability</option>
+            </select>
+            Sort by: 
+            <select name="sort">
+                <option value="">Select One</option>
+                <option value="name">Name</option>
+                <option value="price">Price</option>
+            </select>
+            <input type="submit">
+        </form>
         
+        <?php
        $conn->close();
         ?>
         
