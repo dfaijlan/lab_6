18624f598d122bd8232d1ad66f08d7b8fe2681b0
Dominic Fajilan <dfajilan@csumb.edu>
2017-11-03 05:07:15 +0000

    Lab 6

--- database.php
+++ database.php
@@ -4,7 +4,7 @@
         $host = 'us-cdbr-iron-east-05.cleardb.net';
         $username = 'bea62a3a2d58d2';
         $password = 'c1130949';
-        $dbname = 'tech_devices_app';
+        $dbname = 'heroku_e11c5f3d0a9fe08';
         
         $conn = new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
         return $conn;
